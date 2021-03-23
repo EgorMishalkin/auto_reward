c3ad30248b2df1f685b7ae080cb871dcc9f67e1a
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-03-23 15:56:21 +0400

    Add files via upload

--- design.py
+++ design.py
@@ -18,23 +18,68 @@ def UploadAction(event=None):
 # загрузка файлов в приложение
 
 
+def HelpAction():
+    root = Tk()
+    root['bg'] = '#fafafa'
+    root.title("Автоматические документы")
+    root.geometry('600x300')
+    root.resizable(width=False, height=False)
+    canvas = Canvas(root, height=600, width=300)
+    canvas.pack()
+
+    frame = Frame(root, bg='light blue')
+    frame.place(relwidth=1, relheight=1)
+
+    title = Label(frame, text="Помощь", bg="light blue", font=40)
+    title.pack()
+    lbl1 = Label(frame, text="1). Загрузите с помощью кнопки 'Загрузить 1-ый файл' книгу Excel c расширением .xlsx")
+    lbl1.pack()
+    lbl2 = Label(frame, text="2). Загрузите с помощью кнопки 'Загрузить 2-ой файл' документ Word с расширением .docx")
+    lbl2.pack()
+
+
+def UploadAction1(event=None):
+    # принимаем путь к файлу
+    filename = filedialog.askopenfilename()
+    if str(filename).split('.')[1] in ['xlsx', 'xls']:
+        get_info(filename)
+        notif = Label(frame, text="Награды сделаны!", bg="red")
+        notif.pack()
+    else:
+        notif = Label(frame, text="Данный тип файла не соответствует формату xlsx или xls :(", bg="red")
+        notif.pack()
+
+
+def UploadAction2(event=None):
+    filename2 = filedialog.askopenfilename()
+    if str(filename2).split('.')[1] in ['docx']:
+        print(0)
+    else:
+        print(1)
+
+
 root = Tk()
 root['bg'] = '#fafafa'
 root.title("Автоматические документы")
-root.geometry('500x250')
-canvas = Canvas(root, height=500, width=250)
+root.geometry('600x300')
+root.resizable(width=False, height=False)
+canvas = Canvas(root, height=600, width=300)
 canvas.pack()
 
-frame = Frame(root, bg='red')
-frame.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.7)
+frame = Frame(root, bg='light blue')
+frame.place(relwidth=1, relheight=1)
 # создание текста в окне
-
-title = Label(frame, text="Автоматические документы", bg="red", font=10)
+title = Label(frame, text="Автоматические документы", bg="light blue", font=40)
 title.pack()
 
 # кнопки
+btn = Button(frame, text="Загрузить 1-ый файл", bg="white", command=UploadAction1)
+btn.place(x=320, y=270)
 
-btn = Button(frame, text="Загрузить файл", bg="white", command=UploadAction)
-btn.pack()
+btn2 = Button(frame, text="Загрузить 2-ой файл", bg="white", command=UploadAction2)
+btn2.place(x=470, y=270)
+
+btn3 = Button(frame, text="Помощь", bg="white", command=HelpAction)
+btn3.place(x=4, y=270)
 
 root.mainloop()
