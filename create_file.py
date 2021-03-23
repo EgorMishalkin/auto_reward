1ff0e7c3ad29e196670ff39ffb81d1451e7990ae
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-03-23 12:36:54 +0400

    better design

--- create_file.py
+++ create_file.py
@@ -4,22 +4,34 @@ import shutil
 
 
 # красивые шрифта
-headline1 = ImageFont.truetype('arialbd.ttf', size = 85)
-headline2 = ImageFont.truetype('arial.ttf', size = 20)
-headline3 = ImageFont.truetype('ariblk.ttf', size = 50)
-headline4 = ImageFont.truetype('arial.ttf', size = 27)
-headline5 = ImageFont.truetype('arialbd.ttf', size = 45)
+headline1 = ImageFont.truetype('arialbd.ttf', size=85)
+headline2 = ImageFont.truetype('arial.ttf', size=40)
+headline3 = ImageFont.truetype('ariblk.ttf', size=50)
+headline4 = ImageFont.truetype('arial.ttf', size=27)
+headline5 = ImageFont.truetype('arialbd.ttf', size=45)
 
 
 # делаем файлек
 def create_file(name_folder, type_reward, name_sur, place, reward, event, datetime):
     img = Image.open('Шаблон грамоты.png')
-    
     idraw = ImageDraw.Draw(img)
+
     # о текст мой текст
-    idraw.text((300, 250), type_reward, font=headline1, fill='blue')
+    if len(str(type_reward)) < 10:
+        idraw.text((260, 250), type_reward, font=headline1, fill='blue')
+    else:
+        headline1_2 = ImageFont.truetype('arialbd.ttf', size=70)
+        idraw.text((200, 250), type_reward, font=headline1_2, fill='blue')
+
     idraw.text((290, 370), 'награждается', font=headline2, fill='black')
-    idraw.text((250, 420), name_sur, font=headline3, fill='black')
+
+    if len(str(name_sur)) >= 15:
+        headline3_2 = ImageFont.truetype('ariblk.ttf', size=40)
+        idraw.text((250, 420), name_sur.split(' ')[0], font=headline3_2, fill='black')
+        idraw.text((250, 450), name_sur.split(' ')[1], font=headline3_2, fill='black')
+    else:
+        idraw.text((250, 420), name_sur, font=headline3, fill='black')
+
     idraw.text((250, 550), place, font=headline4, fill='black')
     idraw.text((250, 620), reward, font=headline5, fill='black')
     idraw.text((250, 700), event, font=headline2, fill='black')
