1438f9f81040286ae8126f55da63023ae06b53dc
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-03-23 16:06:42 +0400

    small update

--- create_file.py
+++ create_file.py
@@ -19,7 +19,7 @@ def create_file(name_folder, type_reward, name_sur, place, reward, event, dateti
     convert("шаблон грамоты.docx", "output.pdf")
     os.rename('шаблон грамоты.pdf', f'{name_sur}.pdf')
     # convert("my_docx_folder/")
-    write_to_file(f'{name_sur}.pdf', [name_folder, type_reward, name_sur, place, reward, event, datetime])
+    write_to_file(f'{name_sur}.pdf', [type_reward, name_sur, place, reward, event, datetime])
 
     dest_dir = f'{os.getcwd()}\{name_folder}'
     # move method to move the file
