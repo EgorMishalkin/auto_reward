45632a9f4d0a1c0b92da0a693ffb8aada6169b15
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-03-24 16:14:52 +0400

    pdf fixed

--- create_file.py
+++ create_file.py
@@ -7,18 +7,9 @@ from pdf_work import write_to_file
 
 # делаем файлек
 def create_file(name_folder, dox_place, type_reward, name_sur, place, reward, event, datetime):
-    # convert("шаблон грамоты.docx")
-    # convert("шаблон грамоты.docx", "output.pdf")
-    # os.rename('шаблон грамоты.pdf', f'{name_sur}.pdf')
-    convert(dox_place)
-    convert(dox_place, "output.pdf")
-    os.rename(f'{dox_place.split(".")[0]}.pdf', f'{name_sur}.pdf')
-
-    # convert("my_docx_folder/")
-    write_to_file(f'{name_sur}.pdf', 'shablon_gramoty.pdf', [type_reward, name_sur, place, reward, event, datetime])
-
+    convert(dox_place, "шаблон грамоты.pdf")
+    write_to_file(f'{name_sur}.pdf', 'шаблон грамоты.pdf', [type_reward, name_sur, place, reward, event, datetime])
     dest_dir = f'{os.getcwd()}\{name_folder}'
-    # move method to move the file
     shutil.move(f'{name_sur}.pdf', dest_dir)
 
     # sum_file(f'{name_sur}.pdf', 'шаблон грамоты.pdf', 'merged.pdf')
